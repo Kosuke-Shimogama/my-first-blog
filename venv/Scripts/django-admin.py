af859401f298f4b9dc59fff457adc32420b4077b
Kosuke-Shimogama <k_sg0212@outlook.jp>
2019-05-09 16:31:18 +0900

    My Django first app

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kosuke\Documents\Pycharm\Django\Django_Girls_Tutorial\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
